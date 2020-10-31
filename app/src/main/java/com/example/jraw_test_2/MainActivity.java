7f51a509da414f1955cd80ee43e7d0be6440be77
Luke Brown <luke92brown@gmail.com>
2020-10-31 18:27:33 -0500

    separated AsyncTask and RecyclerView into new Activity RedditViewer

--- app/src/main/java/com/example/jraw_test_2/MainActivity.java
+++ app/src/main/java/com/example/jraw_test_2/MainActivity.java
@@ -1,31 +1,15 @@
 package com.example.jraw_test_2;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.AsyncTask;
+import android.content.Intent;
 import android.os.Bundle;
-
-import net.dean.jraw.RedditClient;
-import net.dean.jraw.http.NetworkAdapter;
-import net.dean.jraw.http.OkHttpNetworkAdapter;
-import net.dean.jraw.http.UserAgent;
-import net.dean.jraw.models.Listing;
-import net.dean.jraw.models.Submission;
-import net.dean.jraw.oauth.Credentials;
-import net.dean.jraw.oauth.OAuthHelper;
-import net.dean.jraw.pagination.DefaultPaginator;
-
-import java.util.ArrayList;
-import java.util.UUID;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView mRecyclerView;
-    private Adapter mAdapter;
-    private ArrayList<Item> mItemList;
-
+    private Button open_reddit_browser_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,78 +18,17 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        // setup recycle viewer
-        mRecyclerView = findViewById(R.id.recycler_view);
-        mRecyclerView.setHasFixedSize(true); // saves memory because size doesn't change
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        // instantiate list?
-        mItemList = new ArrayList<>();
-
-        new MyTask().execute();
-    }
-
-    /*
-        This class is created to allow for networking on the MainActivity thread.
-
-        Sets up all OAuth2 for Reddit API using JRAW. Using JRAW it creates an object
-        that contains subreddit and post data.
-     */
-    private class MyTask extends AsyncTask<Void, Void, Void> {
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-
-            // JRAW client setup
-            UserAgent userAgent = new UserAgent("android", "github.com/lewkyb", "v1", "lookingfordriver");
-            Credentials credentials = Credentials.userlessApp("uKb1rMTs_heYQA", UUID.randomUUID());
-            NetworkAdapter networkAdapter = new OkHttpNetworkAdapter(userAgent);
-            RedditClient redditClient = OAuthHelper.automatic(networkAdapter, credentials);
-
-            // TODO: allow for user to choose subreddits
-            DefaultPaginator<Submission> earthPorn = redditClient.subreddits("aww", "spaceporn").posts().build();
-
-            Listing<Submission> submissions = earthPorn.next();
-            for (Submission s : submissions) {
-
-                // TODO: avoid pulling gif or video
-                if (!s.isSelfPost() && s.getUrl().contains("jpg")) {
-
-                    String imageUrl = s.getUrl();       // URL
-                    System.out.println("imageURL: " +imageUrl);
-
-                    String postTitle = s.getTitle();    // Post Title
-                    System.out.println("postTitle: " + postTitle);
-
-                    int likeCount = s.getScore();       // Upvotes - Downvotes = Score
-                    // add data to Item object
-                    mItemList.add(new Item(imageUrl, postTitle, likeCount));
-                }
+        open_reddit_browser_button = (Button) findViewById(R.id.open_reddit_browser_button);
+        open_reddit_browser_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                open_RedditViewer_activity();
             }
-
-            // not clear on what adapters do yet
-            mAdapter = new Adapter(MainActivity.this, mItemList);
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-
-            /*
-                attempted to run this within the doInBackground(), but
-                it gave the error:
-
-                    android.view.ViewRootImpl$CalledFromWrongThreadException: Only the
-                    original thread that created a view hierarchy can touch its views
-
-                moving this line here fixed the issue, but why? scope?
-             */
-            mRecyclerView.setAdapter(mAdapter);
-
-            super.onPostExecute(aVoid);
-        }
-
+        });
     }
 
+    public void open_RedditViewer_activity() {
+        Intent intent = new Intent(this, RedditViewer.class);
+        startActivity(intent);
+    }
 }
---
+++ app/src/main/java/com/example/jraw_test_2/RedditViewer.java
@@ -0,0 +1,108 @@
+package com.example.jraw_test_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import net.dean.jraw.RedditClient;
+import net.dean.jraw.http.NetworkAdapter;
+import net.dean.jraw.http.OkHttpNetworkAdapter;
+import net.dean.jraw.http.UserAgent;
+import net.dean.jraw.models.Listing;
+import net.dean.jraw.models.Submission;
+import net.dean.jraw.oauth.Credentials;
+import net.dean.jraw.oauth.OAuthHelper;
+import net.dean.jraw.pagination.DefaultPaginator;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class RedditViewer extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private Adapter mAdapter;
+    private ArrayList<Item> mItemList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reddit_viewer);
+
+
+        // setup recycle viewer
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true); // saves memory because size doesn't change
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // instantiate list?
+        mItemList = new ArrayList<>();
+
+        new RedditViewer.MyTask().execute();
+
+    }
+
+    // This task is created to allow for networking on the MainActivity thread.
+    //
+    // Sets up all OAuth2 for Reddit API using JRAW. Using JRAW it creates an object
+    // that contains subreddit and post data.
+    private class MyTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            // JRAW client setup
+            UserAgent userAgent = new UserAgent("android", "github.com/lewkyb", "v1", "lookingfordriver");
+            Credentials credentials = Credentials.userlessApp("uKb1rMTs_heYQA", UUID.randomUUID());
+            NetworkAdapter networkAdapter = new OkHttpNetworkAdapter(userAgent);
+            RedditClient redditClient = OAuthHelper.automatic(networkAdapter, credentials);
+
+            // TODO: allow for user to choose subreddits
+            DefaultPaginator<Submission> earthPorn = redditClient.subreddits("aww", "spaceporn").posts().build();
+
+            Listing<Submission> submissions = earthPorn.next();
+            for (Submission s : submissions) {
+
+                // TODO: avoid pulling gif or video
+                if (!s.isSelfPost() && s.getUrl().contains("jpg")) {
+
+                    String imageUrl = s.getUrl();       // URL
+                    System.out.println("imageURL: " +imageUrl);
+
+                    String postTitle = s.getTitle();    // Post Title
+                    System.out.println("postTitle: " + postTitle);
+
+                    int likeCount = s.getScore();       // Upvotes - Downvotes = Score
+                    // add data to Item object
+                    mItemList.add(new Item(imageUrl, postTitle, likeCount));
+                }
+            }
+
+            // not clear on what adapters do yet
+            mAdapter = new Adapter(RedditViewer.this, mItemList);
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+
+            /*
+                attempted to run this within the doInBackground(), but
+                it gave the error:
+
+                    android.view.ViewRootImpl$CalledFromWrongThreadException: Only the
+                    original thread that created a view hierarchy can touch its views
+
+                moving this line here fixed the issue, but why? scope?
+             */
+            mRecyclerView.setAdapter(mAdapter);
+
+            super.onPostExecute(aVoid);
+        }
+
+    }
+}
